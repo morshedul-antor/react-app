bf22ee063bd2ceaaa2f2b7b3286124c2f3e8b8c6
morshedul-antor <antor.morshedul@gmail.com>
2023-01-04 16:06:42 +0600

    add: new styles

--- App.js
+++ App.js
@@ -2,19 +2,27 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <StatusBar style="auto" />
-            <Text>React Native</Text>
-        </SafeAreaView>
-    );
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text style={styles.text}>New React Native Project</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        backgroundColor: "#fff",
-        marginTop: 40,
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+
+  text: {
+    backgroundColor: "#000",
+    color: "#fff",
+    padding: 20,
+    borderRadius: 4,
+    fontSize: 14,
+  },
 });
